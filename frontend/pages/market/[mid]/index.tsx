7e4a7ea2991dc1b8bfbf51d361506e2f560fc781
Jeppe <s175197@student.dtu.dk>
2022-03-21 18:12:32 +0100

    added in simple error component if something goes wrong when trying to fetch market data.

--- frontend/pages/market/[mid]/index.tsx
+++ frontend/pages/market/[mid]/index.tsx
@@ -3,6 +3,7 @@ import { useRouter } from "next/router";
 import { useContext, useEffect, useState } from "react";
 import { IMarket } from "../../../@types/Market";
 import DateDisplay from "../../../components/DateDisplay";
+import Error from "../../../components/Error";
 import Loading from "../../../components/Loading";
 import { MarketContext } from "../../../stores/Market/MarketStore";
 import { MarketClient } from "../../../stores/models";
@@ -14,6 +15,7 @@ type Props = {
 
 const MarketProfilePageID: NextPage<Props> = () => {
     const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(false);
 
     const [marketId, setMarketId] = useState<string>("");
     const [market, setMarket] = useState<IMarket>(null);
@@ -32,15 +34,20 @@ const MarketProfilePageID: NextPage<Props> = () => {
         if (marketId) {
             var client = new MarketClient();
             client.getMarketInstance(marketId + "").then(
-                res => setMarket({
-                    id: res.marketId,
-                    organiserId: res.organiserId,
-                    name: res.marketName,
-                    startDate: new Date(res.startDate),
-                    endDate: new Date(res.endDate),
-                    description: res.description
-                }));
-                setIsLoading(false);
+                res => {
+                    setMarket({
+                        id: res.marketId,
+                        organiserId: res.organiserId,
+                        name: res.marketName,
+                        startDate: new Date(res.startDate),
+                        endDate: new Date(res.endDate),
+                        description: res.description
+                    });
+                    setIsLoading(false);
+                }).catch(error => {
+                    setError(true);
+                    setIsLoading(false);
+                });
         }
     }, [marketId])
 
@@ -49,31 +56,35 @@ const MarketProfilePageID: NextPage<Props> = () => {
         <div className={styles.profile}>
             <div className={styles.content}>
                 {
-                    isLoading ? <div style={{gridColumnStart: "span 2"}}><Loading /></div> :
-                        <>
-                            <div className={styles.informationContainer}>
-                                {
-                                    (market != undefined || market != null) &&
-                                    <>
-                                        {console.log(market)}
-                                        <div className={styles.contentHeader}>
-                                            <h1>
-                                                {market.name}
-                                            </h1>
-                                            <DateDisplay startDate={market.startDate} endDate={market.endDate} />
-                                        </div>
+                    error ?
+                        <div style={{ gridColumnStart: "span 2" }}>
+                            <Error message={"Ooops Something Went Wrong."} />
+                        </div>
+                        : isLoading ? <div style={{ gridColumnStart: "span 2" }}><Loading /></div>
+                            : <>
+                                <div className={styles.informationContainer}>
+                                    {
+                                        (market != undefined || market != null) &&
+                                        <>
+                                            {console.log(market)}
+                                            <div className={styles.contentHeader}>
+                                                <h1>
+                                                    {market.name}
+                                                </h1>
+                                                <DateDisplay startDate={market.startDate} endDate={market.endDate} />
+                                            </div>
 
-                                        <div className={styles.aboutInfo}>
-                                            {market.description}
-                                        </div>
-                                    </>
-                                }
+                                            <div className={styles.aboutInfo}>
+                                                {market.description}
+                                            </div>
+                                        </>
+                                    }
 
-                            </div>
-                            <div className={styles.mapContainer}>
-                                <div className={styles.mapPlaceholder} />
-                            </div>
-                        </>
+                                </div>
+                                <div className={styles.mapContainer}>
+                                    <div className={styles.mapPlaceholder} />
+                                </div>
+                            </>
                 }
 
             </div>
