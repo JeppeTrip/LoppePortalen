a76eb9f32cf60b46198a04ecd328249576452cf3
Jeppe <s175197@student.dtu.dk>
2022-03-27 12:24:37 +0200

    organisers are loaded from the backend in to the store.

--- frontend/pages/organiser/index.tsx
+++ frontend/pages/organiser/index.tsx
@@ -3,20 +3,31 @@ import { NextPage } from "next";
 import { observer } from "mobx-react-lite";
 import styles from './styles.module.css'
 import { RootStore } from "../../stores/RootStore";
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { StoreContext } from '../../stores/StoreContext'
 import OrganiserListItem from "../../components/OrganiserListItem";
 import TopBar from "../../components/TopBar";
+import ErrorIcon from '@mui/icons-material/Error';
 
 const OrganiserListPage: NextPage = observer(() => {
     const stores = useContext(StoreContext);
 
+    useEffect(() => {
+        stores.organiserStore.loadOrganisers()
+    }, [])
+
     const loading = () => {
         return (
-                <CircularProgress />
+            <CircularProgress />
         )
     }
-     
+
+    const error = () => {
+        return (
+            <ErrorIcon sx={{ fontSize: 50 }} />
+        )
+    }
+
     const content = () => {
         return (
             <List>
@@ -33,14 +44,15 @@ const OrganiserListPage: NextPage = observer(() => {
     }
     return (
         <>
-            <Container                 
-                className={stores.organiserStore.isLoading ? 
-                styles.ContainerLoading : styles.Container} 
+            <Container
+                className={stores.organiserStore.isLoading || stores.organiserStore.hadLoadingError ?
+                    styles.ContainerLoading : styles.Container}
                 maxWidth="sm">
-                    <TopBar />
-                    {
-                    stores.organiserStore.isLoading ? loading() : content()
-                    }
+                <TopBar />
+                {
+                    stores.organiserStore.isLoading ? loading() :
+                        stores.organiserStore.hadLoadingError ? error() : content()
+                }
             </Container>
         </>
     )
--- frontend/stores/Organiser/OrganiserStore.tsx
+++ frontend/stores/Organiser/OrganiserStore.tsx
@@ -2,7 +2,7 @@ import { action, makeAutoObservable } from 'mobx';
 import * as React from 'react';
 import { MarketContextType, IMarket } from '../../@types/Market';
 import { IOrganiser } from '../../@types/Organiser';
-import { MarketClient } from '../models';
+import { MarketClient, OrganiserClient } from '../models';
 import { RootStore } from '../RootStore';
 
 class OrganiserStore {
@@ -10,88 +10,46 @@ class OrganiserStore {
     organisers: IOrganiser[] = [];
 
     isLoading = true;
+    hadLoadingError = false;
 
     constructor(rootStore: RootStore) {
         makeAutoObservable(this);
         this.rootStore = rootStore;
-        //Don't actually think this is a good idea. Since all data will be loaded at once.
-        this.loadOrganisers();
     }
 
     loadOrganisers() {
+        this.hadLoadingError = false;
         this.isLoading = true;
-        //TODO: Replace with a fetch, just have placeholder data for now.
-        this.organisers = [
-            {
-                id : 1,
-                name : "The Best Organiser",
-                description : "The most amazing organiser you have ever seen",
-                street : "Test Street",
-                streetNumber : "123",
-                appartment: "st tv",
-                postalCode : "1234",
-                city : "Test City"
-            },
-            {
-                id : 2,
-                name : "Moria Merchandising",
-                description : "The most amazing organiser you have ever seen",
-                street : "Test Street",
-                streetNumber : "123",
-                appartment: "st tv",
-                postalCode : "1234",
-                city : "Test City"
-            },
-            {
-                id : 3,
-                name : "Rivendell Beauty Parlor",
-                description : "The most amazing organiser you have ever seen",
-                street : "Test Street",
-                streetNumber : "123",
-                appartment: "st tv",
-                postalCode : "1234",
-                city : "Test City"
-            },
-            {
-                id : 4,
-                name : "Mordor IF",
-                description : "The most amazing organiser you have ever seen",
-                street : "Test Street",
-                streetNumber : "123",
-                appartment: "st tv",
-                postalCode : "1234",
-                city : "Test City"
-            },
-            {
-                id : 5,
-                name : "Gondor FC",
-                description : "The most amazing organiser you have ever seen",
-                street : "Test Street",
-                streetNumber : "123",
-                appartment: "st tv",
-                postalCode : "1234",
-                city : "Test City"
-            },
-            {
-                id : 6,
-                name : "Isengard Squash Team",
-                description : "The most amazing organiser you have ever seen",
-                street : "Test Street",
-                streetNumber : "123",
-                appartment: "st tv",
-                postalCode : "1234",
-                city : "Test City"
-            }
-        ]
-        this.isLoading = false;
-
+        const client = new OrganiserClient();
+        client.getAllOrganisers().then(res => {
+            var result = res.map<IOrganiser>(org => {
+                return (
+                    {
+                        //TODO: Fix this so the backend call actually returns all the organiser information, can do this when I get around to needing to show the organiser profile.
+                        id: org.id,
+                        name: org.name,
+                        description: "",
+                        street: "",
+                        streetNumber: "",
+                        appartment: "",
+                        postalCode: "",
+                        city: ""
+                    }
+                )
+            })
+            this.organisers = result
+            this.isLoading = false;
+            this.hadLoadingError = false;
+        }).catch(error => {
+            this.isLoading = false;
+            this.hadLoadingError = true;
+        })
     }
 
     @action
-    addOrganiser(organiser : IOrganiser)
-    {
+    addOrganiser(organiser: IOrganiser) {
         //TODO: Fix this so it calls backend and obviously gets id from there.
-        organiser.id = Math.floor(Math.random()*1000);
+        organiser.id = Math.floor(Math.random() * 1000);
         this.organisers.push(organiser);
         return organiser.id;
     }
