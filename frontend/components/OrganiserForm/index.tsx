e1fda9eefca2da4ff1abeb16905848c86d3a5ea8
Jeppe <s175197@student.dtu.dk>
2022-03-27 11:40:00 +0200

    organiser form commits to the store now.

--- frontend/components/OrganiserForm/index.tsx
+++ frontend/components/OrganiserForm/index.tsx
@@ -49,8 +49,81 @@ const OrganiserForm: FC<Props> = (props: Props) => {
     }
 
     return (
-        <Grid container>
-            
+        <Grid container spacing={1}>
+            <Grid item xs={12}>
+                <TextField
+                    className={styles.nameInput}
+                    id="organiserName"
+                    label="Name"
+                    variant="outlined"
+                    value={newOrganiser.name}
+                    onChange={(event => handleUpdate("name", event.target.value))}
+                />
+            </Grid>
+            <Grid item xs={12}>
+                <TextField
+                    className={styles.nameInput}
+                    id="organiserStreet"
+                    label="Street"
+                    variant="outlined"
+                    value={newOrganiser.street}
+                    onChange={(event => handleUpdate("street", event.target.value))}
+                />
+            </Grid>
+            <Grid item xs={6}>
+                <TextField
+                    className={styles.nameInput}
+                    id="organiserNumber"
+                    label="Street Number"
+                    variant="outlined"
+                    value={newOrganiser.streetNumber}
+                    onChange={(event => handleUpdate("streetNumber", event.target.value))}
+                />
+            </Grid>
+            <Grid item xs={6}>
+                <TextField
+                    className={styles.nameInput}
+                    id="organiserAppartment"
+                    label="Appartment"
+                    variant="outlined"
+                    value={newOrganiser.appartment}
+                    onChange={(event => handleUpdate("appartment", event.target.value))}
+                />
+            </Grid>
+            <Grid item xs={4}>
+                <TextField
+                    className={styles.nameInput}
+                    id="organiserPostal"
+                    label="Postal Code"
+                    variant="outlined"
+                    value={newOrganiser.postalCode}
+                    onChange={(event => handleUpdate("postalCode", event.target.value))}
+                />
+            </Grid>
+            <Grid item xs={8}>
+                <TextField
+                    className={styles.nameInput}
+                    id="organiserCity"
+                    label="City"
+                    variant="outlined"
+                    value={newOrganiser.city}
+                    onChange={(event => handleUpdate("city", event.target.value))}
+                />
+            </Grid>
+            <Grid item xs={12}>
+                <TextField
+                    className={styles.descriptionInput}
+                    id="outlined-multiline-static"
+                    label="Description"
+                    value={newOrganiser.description}
+                    onChange={(event => handleUpdate("description", event.target.value))}
+                    multiline
+                    rows={10}
+                />
+            </Grid>
+            <Grid item>
+                <Button variant="contained" onClick={handleSubmit}>Submit</Button>
+            </Grid>
         </Grid>
 
     )
