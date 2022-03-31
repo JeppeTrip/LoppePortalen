f081bb177f7a984cd9af2918687ec3fa9bdc2ff0
Jeppe <s175197@student.dtu.dk>
2022-03-31 16:11:58 +0200

    set up an app bar that doesn't fuck with my filtering placement.

--- frontend/components/TopBar/index.tsx
+++ frontend/components/TopBar/index.tsx
@@ -1,16 +1,70 @@
 import React, { FC, useContext, useEffect, useState } from 'react';
-import { AppBar, IconButton, Toolbar, Typography } from '@mui/material';
+import { AppBar, Button, Container, Grid, IconButton, Menu, MenuItem, Toolbar, Typography } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
-import styles from './styles.module.css';
 import { StoreContext } from '../../stores/StoreContext';
 import { observer } from 'mobx-react-lite';
+import StorefrontIcon from '@mui/icons-material/Storefront';
+import CorporateFareIcon from '@mui/icons-material/CorporateFare';
+import DeckIcon from '@mui/icons-material/Deck';
+import TableRestaurantIcon from '@mui/icons-material/TableRestaurant';
+import { Box } from '@mui/system';
+import { useRouter } from 'next/router';
 
 type Props = {
 
 }
 
+const menuItems = [
+  {
+    name: "Markets",
+    icon: <DeckIcon />,
+    path: '/market/'
+  },
+  {
+    name: "Stalls",
+    icon: <TableRestaurantIcon />,
+    path: '/stall/'
+  },
+  {
+    name: "Organisers",
+    icon: <CorporateFareIcon />,
+    path: '/organiser/'
+  },
+  {
+    name: "Sellers",
+    icon: <StorefrontIcon />,
+    path: '/seller/'
+  },
+]
+
 const TopBar: FC<Props> = (props: Props) => {
   const stores = useContext(StoreContext);
+  const router = useRouter();
+
+  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
+  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
+
+  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
+    const handleOnClick = (path) => {
+        if (router.isReady) {
+            router.push(path, undefined, { shallow: true });
+        }
+    }
+
 
   return (
     <>
@@ -21,12 +75,71 @@ const TopBar: FC<Props> = (props: Props) => {
           ml: { sm: `${stores.uiStateStore.drawerWidth}px` },
         }}>
         <Toolbar>
-          <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }} onClick={event => { event.preventDefault(); stores.uiStateStore.toggleDrawer() }}>
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" color="inherit" component="div">
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{ mr: 2, display: { xs: 'none', md: 'flex' } }}
+          >
             Loppe Portalen
           </Typography>
+
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                zIndex: 9999,
+                display: { xs: 'block', md: 'none' },
+              }}
+            >
+              {menuItems.map((item) => (
+                <MenuItem key={item.name} onClick={() => handleOnClick(item.path)}>
+                  <Typography textAlign="center">{item.name}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}
+          >
+            Loppe Portalen
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+            {menuItems.map((item) => (
+              <Button
+                key={item.name}
+                onClick={() => handleOnClick(item.path)}
+                sx={{ my: 2, color: 'white', display: 'block' }}
+              >
+                {item.name}
+              </Button>
+            ))}
+          </Box>
         </Toolbar>
       </AppBar>
       <Toolbar />
