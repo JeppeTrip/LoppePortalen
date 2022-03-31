7030fa9ebba3310258017c8fb87513d6f52a4d96
Jeppe <s175197@student.dtu.dk>
2022-03-31 11:30:20 +0200

    made simple filter command.

---
+++ backend/Application/Markets/Queries/GetFilteredMarkets/GetFilteredMarketsQuery.cs
@@ -0,0 +1,62 @@
+﻿using Application.Common.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Markets.Queries.GetFilteredMarkets
+{
+    public class GetFilteredMarketsQuery : IRequest<List<GetFilteredMarketsQueryResponse>>
+    {
+        public GetFilteredMarketsQueryRequest Dto { get; set; }
+
+        public class GetFilteredMarketsQueryHandler : IRequestHandler<GetFilteredMarketsQuery, List<GetFilteredMarketsQueryResponse>>
+        {
+            private readonly IApplicationDbContext _context;
+
+            public GetFilteredMarketsQueryHandler(IApplicationDbContext context)
+            {
+                _context = context;
+            }
+
+            public async Task<List<GetFilteredMarketsQueryResponse>> Handle(GetFilteredMarketsQuery request, CancellationToken cancellationToken)
+            {
+                var instances = await _context.MarketInstances
+                    .Include(x => x.MarketTemplate).ToListAsync();
+
+                if (request.Dto.IsCancelled != null)
+                {
+                    instances = instances.Where(x => x.IsCancelled == request.Dto.IsCancelled).ToList();
+                }
+                if(request.Dto.StartDate != null)
+                {
+                    instances = instances.Where(x => DateTimeOffset.Compare(x.StartDate, (DateTimeOffset)request.Dto.StartDate) >= 0).ToList();
+                }
+                if (request.Dto.EndDate != null)
+                {
+                    instances = instances.Where(x => DateTimeOffset.Compare(x.EndDate, (DateTimeOffset)request.Dto.EndDate) >= 0).ToList();
+                }
+
+                List<GetFilteredMarketsQueryResponse> responses = new List<GetFilteredMarketsQueryResponse>();
+                foreach (var instance in instances)
+                {
+                    responses.Add(new GetFilteredMarketsQueryResponse
+                    {
+                        MarketId = instance.Id,
+                        OrganiserId = instance.MarketTemplate.OrganiserId,
+                        MarketName = instance.MarketTemplate.Name,
+                        Description = instance.MarketTemplate.Description,
+                        StartDate = instance.StartDate,
+                        EndDate = instance.EndDate,
+                        IsCancelled = instance.IsCancelled
+                    });
+                }
+                
+            }
+        }
+    }
+}
