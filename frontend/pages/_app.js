ffacf4a61433b4df7c54b81338f8dc97c3b61a43
Jeppe <s175197@student.dtu.dk>
2022-04-06 14:12:50 +0200

    made sure next isn't yelling about head element no more.

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -17,21 +17,7 @@ function MyApp({ Component, pageProps }) {
       <Script src="https://connect.facebook.net/en_US/sdk.js" strategy="beforeInteractive" />
       <Head>
         <title>Loppe Portalen</title>
-        <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-        />
-        <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/icon?family=Material+Icons"
-        />
         <meta name="viewport" content="initial-scale=1, width=device-width" />
-
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css" />
-        <link rel="stylesheet" href="https://unpkg.com/react-quill@1.3.3/dist/quill.snow.css" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;600&display=swap" rel="stylesheet" />
       </Head>
 
 
