0f64106b302d445a2a2ff2933d265f75aeb08933
Jeppe <s175197@student.dtu.dk>
2022-04-09 15:46:14 +0200

    updated command validator to ensure that user information cannot be empty.

--- backend/Application/User/Commands/RegisterUser/RegisterUserCommandValidator.cs
+++ backend/Application/User/Commands/RegisterUser/RegisterUserCommandValidator.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Application.User.Commands.CreateUser;
+using FluentValidation;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,24 @@ using System.Threading.Tasks;
 
 namespace Application.User.Commands.RegisterUser
 {
-    public class RegisterUserCommandValidator
+    public class RegisterUserCommandValidator : AbstractValidator<RegisterUserCommand>
     {
+        public RegisterUserCommandValidator()
+        {
+            RuleFor(e => e.Dto)
+                .NotNull();
+
+            RuleFor(e => e.Dto.Email)
+                .NotEmpty();
+
+            RuleFor(e => e.Dto.Password)
+                .NotEmpty();
+
+            RuleFor(e => e.Dto.FirstName)
+                .NotEmpty();
+
+            RuleFor(e => e.Dto.LastName)
+                .NotEmpty();
+        }
     }
 }
