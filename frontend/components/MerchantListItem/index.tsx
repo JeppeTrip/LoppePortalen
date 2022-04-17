0cb52340c53a38c27bffbc4d38a1067cabe95b57
Jeppe <s175197@student.dtu.dk>
2022-04-17 14:59:28 +0200

    added in a merchant list item that is now shown in the merchants list.

--- frontend/components/MerchantListItem/index.tsx
+++ frontend/components/MerchantListItem/index.tsx
@@ -1,23 +1,32 @@
 import StoreIcon from '@mui/icons-material/Store';
-import { ListItem, ListItemAvatar, ListItemText } from "@mui/material";
+import { ListItem, ListItemAvatar, ListItemButton, ListItemText } from "@mui/material";
 import { observer } from "mobx-react-lite";
-import { FC } from "react";
+import { useRouter } from 'next/router';
+import { FC, useCallback } from "react";
 import { Merchant } from '../../NewStores/@DomainObjects/Merchant';
 type Props = {
     merchant: Merchant
 }
 
-
-
 const MerchantListItem: FC<Props> = (props: Props) => {
+    const router = useRouter()
+
+    const redirect = useCallback(() => {
+        if (router.isReady)
+            router.push(`merchant/${props.merchant.id}`, undefined, { shallow: true })
+    }, [router.isReady])
+
     return (
         <ListItem>
-            <ListItemAvatar>
-                <StoreIcon />
-            </ListItemAvatar>
-            <ListItemText
-                primary={props.merchant.name}
-            />
+            <ListItemButton onClick={() => redirect()}>
+                <ListItemAvatar>
+                    <StoreIcon />
+                </ListItemAvatar>
+                <ListItemText
+                    primary={props.merchant.name}
+                />
+            </ListItemButton>
+
 
         </ListItem >
     );
