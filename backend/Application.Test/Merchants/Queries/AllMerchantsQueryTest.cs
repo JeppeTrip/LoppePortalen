ecfeb8dfba5186e9e448e028f125a28759721d28
Jeppe <s175197@student.dtu.dk>
2022-04-26 16:51:22 +0200

    tested that all merchants query actually return all merchants.

---
+++ backend/Application.Test/Merchants/Queries/AllMerchantsQueryTest.cs
@@ -0,0 +1,25 @@
+﻿using Application.Merchants.Queries.AllMerchants;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Application.Test.Merchants.Queries
+{
+    public class AllMerchantsQueryTest : TestBase
+    {
+        [Fact]
+        public async Task Handle_GetAllMerchants()
+        {
+            var request = new AllMerchantsQuery();
+            var handler = new AllMerchantsQuery.AllMerchantsQueryHandler(Context);
+            var result = await handler.Handle(request, CancellationToken.None);
+            result.MerchantList.Should().NotBeEmpty();
+            result.MerchantList.Count.Should().Be(Context.Merchants.Count());
+        }
+    }
+}
