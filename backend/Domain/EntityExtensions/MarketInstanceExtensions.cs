afb129bdfd10e47de6deb0e7f5456a343a9366c3
Jeppe <s175197@student.dtu.dk>
2022-04-28 15:29:36 +0200

    setup the entity extension methods.

---
+++ backend/Domain/EntityExtensions/MarketInstanceExtensions.cs
@@ -0,0 +1,33 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.EntityExtensions
+{
+    public static class MarketInstanceExtensions
+    {
+        public static int TotalStallCount(this MarketInstance instance)
+        {
+            return 0;
+        }
+
+        public static int AvailableStallCount(this MarketInstance instance)
+        {
+            return 0;
+        }
+
+        public static int OccupiedStallCount(this MarketInstance instance)
+        {
+            return 0;
+        }
+
+        public static List<string> ItemCategories(this MarketInstance instance)
+        {
+            var list = new List<string>();
+            return list;
+        }
+    }
+}
