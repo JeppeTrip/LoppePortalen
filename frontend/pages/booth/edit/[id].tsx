dc1a01ceaa59936cc60825766f807c876e12f1dc
Jeppe <s175197@student.dtu.dk>
2022-05-08 13:55:55 +0200

    edit booth page content moved into booth form component.

--- frontend/pages/booth/edit/[id].tsx
+++ frontend/pages/booth/edit/[id].tsx
@@ -9,6 +9,7 @@ import { useContext, useEffect, useState } from "react";
 import { useErrorHandler } from "react-error-boundary";
 import { ModelState } from '../../../@types/ModelState';
 import { NextPageAuth } from "../../../@types/NextAuthPage";
+import BoothForm from "../../../components/BoothForm";
 import { Booth } from "../../../NewStores/@DomainObjects/Booth";
 import { StoreContext } from '../../../NewStores/StoreContext';
 
@@ -18,21 +19,15 @@ type Props = {
 }
 
 const EditBoothPage: NextPageAuth<Props> = observer(() => {
-    const handleError = useErrorHandler()
-    const [tabValue, setTabValue] = useState('1')
-    const [selectedBooth, setSelectedBooth] = useState<Booth>(null)
     const stores = useContext(StoreContext);
-    const [boothId, setBoothId] = useState<string>("");
+    const handleError = useErrorHandler()
     const router = useRouter();
+    const [selectedBooth, setSelectedBooth] = useState<Booth>(null)
+    const [boothId, setBoothId] = useState<string>("");
 
     //mount
     useEffect(() => {
         stores.itemCategoryStore.fetchCategories()
-    }, [])
-
-    //Unmount
-    useEffect(() => () => {
-
     }, [])
 
     useEffect(() => {
@@ -43,9 +38,6 @@ const EditBoothPage: NextPageAuth<Props> = observer(() => {
         setBoothId(id + "")
     }, [router.isReady]);
 
-    /**
-     * If selected market is empty in the stores search for it.
-     */
     useEffect(() => {
         if (selectedBooth == null) {
             if (!(boothId === "")) {
@@ -60,10 +52,6 @@ const EditBoothPage: NextPageAuth<Props> = observer(() => {
         }
     }, [boothId, selectedBooth])
 
-    const handleTabChange = (event: React.SyntheticEvent, newValue: string) => {
-        setTabValue(newValue);
-    };
-
     return (
         <Container>
             <Paper elevation={1} sx={{ p: 2 }}>
@@ -71,81 +59,7 @@ const EditBoothPage: NextPageAuth<Props> = observer(() => {
                     Edit Boothpage
                 </Typography>
                 {
-                    selectedBooth != null && (
-                        <>
-                            <Grid container spacing={2}>
-                                <Grid item xs={12}>
-                                    <TextField
-                                        fullWidth={true}
-                                        id="marketName"
-                                        label="Name"
-                                        variant="outlined"
-                                        onChange={(event) => selectedBooth.name = event.target.value}
-                                        value={selectedBooth.name} />
-                                </Grid>
-
-                                <Grid item xs={12}>
-                                    <TextField
-                                        fullWidth
-                                        id="outlined-multiline-static"
-                                        label="Description"
-                                        value={selectedBooth.description}
-                                        onChange={(event) => selectedBooth.description = event.target.value}
-                                        multiline
-                                        rows={10}
-                                    />
-                                </Grid>
-                                <Grid item xs={12}>
-                                    <Autocomplete
-                                        onChange={(event,value) => selectedBooth.itemCategories = value}
-                                        fullWidth
-                                        multiple
-                                        id="tags-standard"
-                                        value={selectedBooth.itemCategories}
-                                        loading={stores.itemCategoryStore.categories.length === 0}
-                                        loadingText={"Fetching categories..."}
-                                        options={
-                                            stores.itemCategoryStore.categories
-                                        }
-                                    getOptionLabel={(option) => option}
-                                    renderInput={(params) => (
-                                        <TextField
-                                            {...params}
-                                            variant="standard"
-                                            label="Item Categories"
-                                            placeholder="Item Categories"
-                                        />
-                                    )}
-                                    />
-                                </Grid>
-                            </Grid>
-                            <Grid>
-                                <LoadingButton
-                                    onClick={() => selectedBooth.save()}
-                                    loading={selectedBooth.state === ModelState.SAVING}
-                                    loadingPosition="start"
-                                    startIcon={<SaveIcon />}
-                                    variant="contained"
-                                    sx={{ mt: 3, ml: 1 }}
-                                >
-                                    Update
-                                </LoadingButton>
-                            </Grid>
-                            {
-                                selectedBooth.state === ModelState.ERROR &&
-                                (
-                                    <Grid>
-                                        <Grid item>
-                                            <Typography variant="caption" color="red">
-                                                Something went wrong.
-                                                Unable to submit new market.
-                                            </Typography>
-                                        </Grid>
-                                    </Grid>
-                                )
-                            }
-                        </>
-                    )
+                    selectedBooth != null && <BoothForm booth={selectedBooth} />
                 }
             </Paper>
         </Container>
