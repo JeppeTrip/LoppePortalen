84a63fba52cd02971e71606139322bba8c8ddd96
Jeppe <s175197@student.dtu.dk>
2022-05-10 13:51:43 +0200

    added address informatoin to market model.

--- backend/Domain/Entities/MarketTemplate.cs
+++ backend/Domain/Entities/MarketTemplate.cs
@@ -12,6 +12,9 @@ namespace Domain.Entities
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+        public string Address { get; set; }
+        public string PostalCode { get; set; }
+        public string City { get; set; }
 
         public int OrganiserId { get; set; }
         public virtual Organiser Organiser { get; set; }
